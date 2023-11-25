e051ae3880a294467f12698f3add92f8a1aefe09
dussy42 <68313949+mommoh@users.noreply.github.com>
2023-11-25 04:18:19 +0100

    ye
    yes

--- momor_env/momorPet/userprofile/tests/test_userprofile_models.py
+++ momor_env/momorPet/userprofile/tests/test_userprofile_models.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from yourapp.models import UserProfile  # Replace 'yourapp' with your actual app name
+# Replace 'yourapp' with your actual app name
+from yourapp.models import UserProfile
+
 
 class UserProfileModelTest(TestCase):
     def setUp(self):
-        
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
 
     def test_userprofile_creation(self):
-        
+
         user_profile = UserProfile.objects.create(
             user=self.user,
             bio='This is a test bio.',
-           
+
         )
 
-       
         saved_profile = UserProfile.objects.get(user=self.user)
 
-        
         self.assertEqual(saved_profile.user, self.user)
         self.assertEqual(saved_profile.bio, 'This is a test bio.')
-       
