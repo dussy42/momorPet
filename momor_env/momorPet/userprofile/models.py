75a056a742914a0c1799e1635c85ecd9455cfe4d
dussy42 <dussy47@gmail.com>
2023-11-25 04:27:00 +0100

    model
    adjustment

--- momor_env/momorPet/userprofile/models.py
+++ momor_env/momorPet/userprofile/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Userprofile(models.Model):
     user = models.OneToOneField(
         User, related_name='userprofile', on_delete=models.CASCADE)
-    is_vendor = modls.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
